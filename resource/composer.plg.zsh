fe2d9182d750b7319d2d835541ccba98ffebdfbd
inhere <in.798@qq.com>
2018-07-02 19:32:50 +0800

    add some example shell auto-complete files

---
+++ resource/composer.plg.zsh
@@ -0,0 +1,35 @@
+# ------------------------------------------------------------------------------
+#          FILE:  composer.plugin.zsh
+#   DESCRIPTION:  oh-my-zsh composer plugin file.
+#        AUTHOR:  Daniel Gomes (me@danielcsgomes.com)
+#       VERSION:  1.0.0
+# ------------------------------------------------------------------------------
+
+# Composer basic command completion
+_composer_get_command_list () {
+    $_comp_command1 --no-ansi 2>/dev/null | sed "1,/Available commands/d" | awk '/^[ \t]*[a-z]+/ { print $1 }'
+}
+
+_composer_get_required_list () {
+    $_comp_command1 show -s --no-ansi 2>/dev/null | sed '1,/requires/d' | awk 'NF > 0 && !/^requires \(dev\)/{ print $1 }'
+}
+
+_composer () {
+  local curcontext="$curcontext" state line
+  typeset -A opt_args
+  _arguments \
+    '1: :->command'\
+    '*: :->args'
+
+  case $state in
+    command)
+      compadd $(_composer_get_command_list)
+      ;;
+    *)
+      compadd $(_composer_get_required_list)
+      ;;
+  esac
+}
+
+compdef _composer composer
+compdef _composer composer.phar
