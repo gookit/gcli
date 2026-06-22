d264aaf5caf740d037a331b0d5dc5b89e8d8601c
inhere <in.798@qq.com>
2026-06-22 09:07:22 +0800

    chore: revert deleted feature codes

--- builtin/gen_gcli_command.go
+++ builtin/gen_gcli_command.go
@@ -2,7 +2,7 @@ package builtin
 
 import "github.com/gookit/gcli/v3"
 
-// GenCmdCode command
+// GenCmdCode command TODO
 var GenCmdCode = &gcli.Command{
 	Name: "gen-cmd",
 	Desc: "quick generate gcli command code",
